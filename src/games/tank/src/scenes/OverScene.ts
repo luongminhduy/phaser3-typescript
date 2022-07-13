d0dec8e497de267a0a4f2f707e416c5ed9106477
luongminhduy <87934522+luongminhduy@users.noreply.github.com>
2022-07-13 16:59:36 +0700

    fix game over scene

--- src/games/tank/src/scenes/OverScene.ts
+++ src/games/tank/src/scenes/OverScene.ts
@@ -1,3 +1,4 @@
+import { Button } from "../objects/Button";
 export class OverScene extends Phaser.Scene {
     private bitmapTexts: Phaser.GameObjects.BitmapText[] = [];
     private containerButtonNewGame:  Phaser.GameObjects.Container;
@@ -7,74 +8,54 @@ export class OverScene extends Phaser.Scene {
         });
     }
 
-    addButtonNew() {
-      let newLabel = this.add.image(0, 0, 'buttonNew').setOrigin(0.5, 0.5);
-      let newText = this.add.bitmapText(0, 0, 'mainFont', 'New Game', 40).setOrigin(0.5, 0.5);
-      this.containerButtonNewGame = this.add.container(this.sys.canvas.width / 2, 700, [ newLabel, newText ]).setScrollFactor(0);
-      this.containerButtonNewGame.setSize(newLabel.width, newLabel.height);
-      this.containerButtonNewGame.setInteractive();
-      
-      this.containerButtonNewGame.on('pointerover', function() {
-        newLabel.setTint(0x44ff44);
-      });
-      this.containerButtonNewGame.on('pointerout', function () {
-
-          newLabel.clearTint();
-  
-      });
-  
-      this.containerButtonNewGame.on('pointerdown',  () => {
-        this.scene.stop('OverScene');
-        this.scene.start('GameScene');
-      });
+    create() {
+      //Game Over Text
+      this.addText(this.sys.canvas.width / 2 - 100, 40, 'Game Over');
+      //Your score text
+      this.addText(this.sys.canvas.width / 2 - 120, 100, 'Your Score is ');
+      //score
+      this.addScore(this.sys.canvas.width / 2 - 80, 200, 'score');
+      //Highest score text
+      this.addText(this.sys.canvas.width / 2 - 120, 400, 'Highest Score ');
+      //Highest
+      this.addScore(this.sys.canvas.width / 2 - 80, 500, 'highest');
+      //Button for new game
+      this.addButtonNew();
     }
 
-    create() {
-        this.bitmapTexts.push(
-            this.add.bitmapText(
-              this.sys.canvas.width / 2 - 100,
-              40,
-              'font',
-              'Game Over',
-              30
-            )
-        );
-        this.bitmapTexts.push(
-            this.add.bitmapText(
-              this.sys.canvas.width / 2 - 120,
-              100,
-              'font',
-              'Your Score is ',
-              30
-            )
-        );
-        this.bitmapTexts.push(
-            this.add.bitmapText(
-              this.sys.canvas.width / 2 - 80,
-              200,
-              'scoreFont',
-              `${this.registry.get('score')}`,
-              80
-            )
-        );
-        this.bitmapTexts.push(
-            this.add.bitmapText(
-              this.sys.canvas.width / 2 - 120,
-              400,
-              'font',
-              'Highest Score ',
-              30
-            )
-        );
-        this.bitmapTexts.push(
-            this.add.bitmapText(
-              this.sys.canvas.width / 2 - 80,
-              500,
-              'scoreFont',
-              `${this.registry.get('highest')}`,
-              80
-            )
-        );
-        this.addButtonNew();
+    private addButtonNew() {
+      const buttonX = this.sys.canvas.width / 2;
+      const buttonY = 700;
+      let button = new Button(buttonX, buttonY, this, 'New Game');
+      this.containerButtonNewGame = button.create(this);
+      button.setInteract();
+      button.container.on('pointerdown', () => {
+        this.scene.stop('OverScene');
+        this.scene.start('GameScene');       
+      })
+    }
+
+    private addText(x: number, y: number, text: string) {
+      this.bitmapTexts.push(
+        this.add.bitmapText(
+          x,
+          y,
+          'font',
+          text,
+          30
+        )
+      );
+    }
+
+    private addScore(x: number, y: number, type: string) {
+      this.bitmapTexts.push(
+        this.add.bitmapText(
+          x,
+          y,
+          'scoreFont',
+          `${this.registry.get(type)}`,
+          80
+        )
+      );
     }
 }
